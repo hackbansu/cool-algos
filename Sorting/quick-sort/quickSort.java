476cd21989d654374b33fc3922032e5613cf0751
pawan bansal <bansal.pawan96@gmail.com>
2020-10-02 16:16:27 +0530

    fixes

--- Sorting/quick-sort/quickSort.java
+++ Sorting/quick-sort/quickSort.java
@@ -1,17 +1,35 @@
 import java.io.*;
+import java.util.*;
 
 public class Quick_Sort {
   public static void main(String[] args) {
-    int[] arr = {5, 7, 3, 10, 2000, 9, 19, 16};
+    int[] arr = takeinput();
 
     // sort the array
     Quick_Sort(arr, 0, arr.length - 1);
 
     // display the array
     for (int i = 0; i < arr.length; i++)
-      System.out.print(arr[i] + ", ");
-    System.out.println("End");
+      System.out.print(arr[i] + " ");
   }
+
+  public static int[] takeinput()
+	{
+		Scanner scn=new Scanner(System.in);
+		
+		// input array length
+		int n=scn.nextInt();
+		
+		int[] arr = new int[n];
+		
+		for(int i=0;i<arr.length;i++)
+		{
+			// input array elements
+			arr[i]=scn.nextInt();
+		}
+		
+		return arr;
+	}
 
   public static void Quick_Sort(int[] arr, int lo, int hi) {
     // if low index is higher than high index, simply return
